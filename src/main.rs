758a6a918c8d8e1ca1eaca942c7700a9f16ca00b
John Schmidt <john@john2143.com>
2020-11-19 23:07:43 -0500

    read data into gallery list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+#[derive(Debug)]
+struct Gallery<'a> {
+    name: &'a str,
+    pics: Vec<&'a str>,
+}
+
+fn main() -> Result<()> {
+    let data = std::fs::read_to_string("./data.txt").unwrap();
+
+    let mut current_gallery: Option<Gallery> = None;
+    let mut galleries = vec![];
+    for line in data.lines() {
+        if let Some(gal) = &mut current_gallery {
+            if line == "" {
+                galleries.push(current_gallery.take().unwrap());
+            } else {
+                gal.pics.push(line);
+            }
+        } else {
+            current_gallery = Some(Gallery {
+                name: line,
+                pics: vec![],
+            });
+        }
+    }
+
+    if let Some(x) = current_gallery.take() {
+        galleries.push(x);
+    }
+
+    dbg!(&galleries);
+
+    Ok(())
 }
